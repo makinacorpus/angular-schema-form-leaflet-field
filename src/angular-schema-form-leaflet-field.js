b89810bf76214c601d2020f58b9900753cde7e64
Simon Prévidente <simon.previdente@free.fr>
2015-07-03 16:00:00 +0200

    Added: Leaflet draggable marker

--- src/angular-schema-form-leaflet-field.js
+++ src/angular-schema-form-leaflet-field.js
@@ -19,10 +19,18 @@ angular.module("schemaForm")
 
 					// Leaflet
 					$timeout(function() {
+						// Map
 						var map = L.map(mapDivId).setView([43.575137,1.406327], 16);
 						L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 							attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 						}).addTo(map);
+
+						// Marker
+						var marker = L.marker(new L.LatLng(43.575137,1.406327), {
+							draggable: true
+						});
+						marker.bindPopup('This marker is draggable! Move it around.');
+						marker.addTo(map);
 					});
 				});
 			}]
