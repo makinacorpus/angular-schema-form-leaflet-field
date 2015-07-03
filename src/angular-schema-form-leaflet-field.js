9c6ba1a5d1acf96c1738c4311f28415fed864201
Simon Prévidente <simon.previdente@free.fr>
2015-07-03 15:30:00 +0200

    Fixed #2

--- src/angular-schema-form-leaflet-field.js
+++ src/angular-schema-form-leaflet-field.js
@@ -9,21 +9,22 @@ angular.module("schemaForm")
 			"build/leaflet-draggable-marker.html");
 	}])
 
-	.directive('leafletDraggableMarker', function() {
+	.directive('leafletDraggableMarker', ['$timeout', function($timeout) {
 		return {
 			controller: ['$scope', function($scope) {
-
 				$scope.$watch($scope.form, function() {
 					// Form key
 					var mapDivId = $scope.form.key[0];
 					console.log(mapDivId);
-					// Leaflet
-					var map = L.map(mapDivId).setView([0,0], 16);
-					L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-						attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-					}).addTo(map);
 
+					// Leaflet
+					$timeout(function() {
+						var map = L.map(mapDivId).setView([43.575137,1.406327], 16);
+						L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+							attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+						}).addTo(map);
+					});
 				});
 			}]
 		};
-	});
+	}]);
